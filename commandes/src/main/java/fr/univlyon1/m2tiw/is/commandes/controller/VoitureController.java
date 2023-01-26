2c56544c27894f6abe15ade6021b96b79de8e23f
Castaldo Laetitia <laetitia.castaldo@etu.univ-lyon1.fr>
2023-01-26 16:57:05 +0100

    Modif voiturecONTROLLER

--- commandes/src/main/java/fr/univlyon1/m2tiw/is/commandes/controller/VoitureController.java
+++ commandes/src/main/java/fr/univlyon1/m2tiw/is/commandes/controller/VoitureController.java
@@ -1,9 +1,48 @@
 package fr.univlyon1.m2tiw.is.commandes.controller;
+import fr.univlyon1.m2tiw.is.commandes.dao.CommandeDAOImpl;
+import fr.univlyon1.m2tiw.is.commandes.dao.NotFoundException;
+import fr.univlyon1.m2tiw.is.commandes.dao.VoitureDAO;
+import fr.univlyon1.m2tiw.is.commandes.dao.VoitureDAOImpl;
+import fr.univlyon1.m2tiw.is.commandes.model.Commande;
+import fr.univlyon1.m2tiw.is.commandes.model.Option;
+import fr.univlyon1.m2tiw.is.commandes.model.Voiture;
+import fr.univlyon1.m2tiw.is.commandes.services.CommandeCouranteServiceImpl;
+import fr.univlyon1.m2tiw.is.commandes.services.VoitureService;
+import fr.univlyon1.m2tiw.is.commandes.services.VoitureServiceImpl;
 
+import java.sql.SQLException;
+import java.util.Collection;
 
 public class VoitureController {
 
-    public String test() {
-        return "hello";
+    private static int counter;
+    private VoitureServiceImpl voitureService;
+    private CommandeCouranteServiceImpl commandeCouranteService;
+    private VoitureDAOImpl voitureDAO;
+    private CommandeDAOImpl commandeDAO;
+
+
+    public Voiture creerVoiture(String modele){
+        try{
+            return voitureService.creerVoiture(modele);
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+         return null;
     }
+
+    public void ajouterConfiguration(Voiture voiture, Option option) throws SQLException, NotFoundException {
+        try{
+            voitureService.ajouterConfiguration(voiture.getId(), option);
+        }catch (SQLException | NotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void supprimerVoiture(Voiture voiture) throws SQLException, NotFoundException {
+        voitureService.supprimerVoiture(voiture.getId());
+    }
+
+
+
 }
