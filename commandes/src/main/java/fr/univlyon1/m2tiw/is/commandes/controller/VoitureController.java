0bd77a21c3f44bbc516ed5aeecac17375d5ac034
JeanSar <jeansar504@gmail.com>
2023-01-26 16:10:24 +0100

    Ca devrait marcher lol

--- commandes/src/main/java/fr/univlyon1/m2tiw/is/commandes/controller/VoitureController.java
+++ commandes/src/main/java/fr/univlyon1/m2tiw/is/commandes/controller/VoitureController.java
@@ -1,16 +1,9 @@
 package fr.univlyon1.m2tiw.is.commandes.controller;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping(path = "/")
 public class VoitureController {
-    @GetMapping()
-    public ResponseEntity<String> test() {
-        return new ResponseEntity<String>("hello", HttpStatus.OK);
+
+    public String test() {
+        return "hello";
     }
 }
