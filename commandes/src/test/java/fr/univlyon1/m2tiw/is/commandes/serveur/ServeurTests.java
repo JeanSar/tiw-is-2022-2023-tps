ad9ebe9e2f90424e6f563660cfeb02407b127f22
JeanSar <jeansar504@gmail.com>
2023-01-28 15:07:21 +0100

    Ajout d'un fichier de test pour le serveur

---
+++ commandes/src/test/java/fr/univlyon1/m2tiw/is/commandes/serveur/ServeurTests.java
@@ -0,0 +1,25 @@
+package fr.univlyon1.m2tiw.is.commandes.serveur;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ServeurTests {
+    private static Logger log = LoggerFactory.getLogger(fr.univlyon1.m2tiw.is.commandes.serveur.ServeurTests.class);
+    private Serveur serveur;
+
+    @BeforeEach
+    public void before() {
+        serveur = new Serveur();
+    }
+
+    @Test
+    public void getAllOptions() {
+        var options = serveur.getAllOptions();
+        assertTrue(1 > options.size());
+    }
+
+}
